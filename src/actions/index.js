9c513298f305438f33e6c94eff39bcebb3d3f7ca
Oleg5107767 <github.com/Oleg5107767/my-test-.git>
2022-07-02 11:03:07 +0300

    createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,5 @@
-import { heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice';
-import { filtersFetching, filtersFetched, filtersFetchingError } from '../components/heroesFilters/filtersSlice';
 
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
+
 //export const heroesFetching = createAction('HEROES_FETCHING')
 //export const heroesFetched = createAction('HEROES_FETCHED')
 //export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
@@ -47,12 +40,7 @@ export const fetchHeroes = (request) => (dispatch) => {
 //}
 
 
-export const fetchFilters = (request) => (dispatch) => {
-    dispatch(filtersFetching());
-    request("http://localhost:3001/filters")
-        .then(data => dispatch(filtersFetched(data)))
-        .catch(() => dispatch(filtersFetchingError()))
-}
+
 
 //export const filtersFetching = () => {
 //    return {
--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { useHttp } from '../../hooks/http.hook';
+
 
 const initialState = {
     filters: [],
@@ -6,6 +8,14 @@ const initialState = {
     activeFilter: 'all'
 }
 
+
+export const fetchFilters = createAsyncThunk(
+    'filters/fetchFilters',
+    async () => {
+        const {request} = useHttp();
+        return await request("http://localhost:3001/filters");
+    }
+)
 const filtersSlice = createSlice({
     name: 'filters',
     initialState,
@@ -20,7 +30,19 @@ const filtersSlice = createSlice({
         },
         filtersChanged: (state, action) => {
             state.activeFilter = action.payload;
-        }
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
+        .addCase(fetchFilters.fulfilled, (state, action) => {
+            state.filtersLoadingStatus = 'idle';
+            state.filters = action.payload;
+        })
+        .addCase(fetchFilters.rejected,  state => {
+            state.filtersLoadingStatus = 'error';
+        })
+        .addDefaultCase(() => {})
     }
 });
 
